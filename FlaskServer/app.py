4bd0e81ae4f5bd50257ec0f170a96cbc16384e27
spectr747 <spectr747@gmail.com>
2024-10-26 13:50:18 +0300

    Update flask

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -4,7 +4,7 @@ from joblib.parallel import method
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -29,24 +29,15 @@ kNN = pickle.load(open('../FlaskServer/models/kNN.pkl', 'rb'))
 #get_data
 
 
-def predict() -> str:
+def predict(client_data) -> str:
     """
     Прогноз
 
     :return: Результат yes/no
     """
     ##categorical
-    X_cat_from_keyboard = ['unemployed',
-                           'married',
-                           'primary',
-                           'no',
-                           'no',
-                           'yes',
-                           'cellular',
-                           'may',
-                           'failure'
-                           ]
-    
+    X_cat_from_keyboard = client_data[0][0:9]
+    print(X_cat_from_keyboard)
     # print(X_cat_from_keyboard)
     le_list = [job_LE,
                marital_LE,
@@ -64,14 +55,8 @@ def predict() -> str:
         X_le_list.append(x_cat)
     print('X_cat_le:', X_le_list)
     ##num
-    
-    X_nums_from_keyboard =[30,
-                           1787,
-                           16,
-                           199,
-                           4,
-                           330,
-                           0]
+
+    X_nums_from_keyboard = client_data[0][9:]
     print('X_nums', X_nums_from_keyboard)
     #объединить категориальные и числовые (в том же порядке, как и при обучении)
     X = []
@@ -85,12 +70,14 @@ def predict() -> str:
     prediction = kNN.predict(X_scaled)
     print(prediction)
     #result
-    result = y_LE.inverse_transform(prediction)
+    result = y_LE.inverse_transform(prediction)[0]
     return result
 
 @app.route("/api/v1/get_data/", methods=["GET", "POST"])
 def get_data():
-    return jsonify(str(predict()))
+    client_data = request.json['client_form'] #получаем json от клиента
+
+    return jsonify({'result': predict(client_data)})
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
