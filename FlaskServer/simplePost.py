f83c6278b1f53af56768fd32a754f96260cf5678
lasttrader <grishaperm@list.ru>
2024-10-26 13:54:34 +0300

    added fixes

--- FlaskServer/simplePost.py
+++ FlaskServer/simplePost.py
@@ -1,5 +1,24 @@
 import requests
 
-api_message = requests.post('http://127.0.0.1:5000/api/v1/get_data/')
+
+client_form = ['unemployed',
+                           'married',
+                           'primary',
+                           'no',
+                           'no',
+                           'yes',
+                           'cellular',
+                           'may',
+                           'failure',
+                           30,
+                           1787,
+                           16,
+                           199,
+                           4,
+                           330,
+                           0]
+
+api_message = requests.post('http://127.0.0.1:5000/api/v1/get_data/',
+                            json = {'client_form': [client_form]})
 if api_message.ok:
     print(api_message.json())
